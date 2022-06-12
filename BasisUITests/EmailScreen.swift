ae299aed92864f1137b1e012970ce550f7d01cf9
joysonjson <joysonpathra@gmail.com>
2022-06-13 00:12:45 +0530

    finalising

---
+++ BasisUITests/EmailScreen.swift
@@ -0,0 +1,41 @@
+//
+//  EmailScreen.swift
+//  BasisUITests
+//
+//  Created by Joyson P S on 12/06/22.
+//
+
+import XCTest
+@testable import Basis
+
+class EmailScreen: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testEmailTextFieldPresentWithEmptyValue() throws {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+//        let storyBoard = UIStoryboard(name: "Login", bundle: nil)
+//        let vc = storyBoard.instantiateViewController(withIdentifier: "EmailViewController") as! EmailViewController
+//        vc.loadViewIfNeeded()
+//        XCTAssertEqual(vc.emailTextField.text, "")
+        
+        
+    }
+
+}
